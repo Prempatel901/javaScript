b1ae8d2981714c105ecb228e6b94e5e529fd3743
Prem <premkumar33652@gmail.com>
2026-04-02 22:46:06 +0530

    0.2 Even Or Odd

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 
 
 
-let a = 10
-let b = 20
-let c
-console.log("a = ",a,"b = ",b)
-c=a;
-a=b
-b=c
+// let a = 10
+// let b = 20
+// let c
+// console.log("a = ",a,"b = ",b)
+// c=a;
+// a=b
+// b=c
 
-console.log("a = ",a,"b = ",b)
+// console.log("a = ",a,"b = ",b)
+
+
+
+
+
+let i ;
+
+for (i = 1; i <= 10; i++) {
+
+    if (i % 2 === 0) {
+        console.log(i," Even ")
+    } else {
+        console.log(i," Odd")
+    }
+
+}
