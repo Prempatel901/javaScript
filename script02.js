bd9fce59e6e568459f9ddb9ce3f5f6062fdedc6f
Prem <premkumar33652@gmail.com>
2026-04-03 22:33:24 +0530

    Number

---
+++ script02.js
@@ -0,0 +1,64 @@
+// Operator
+
+// Arithemetic
+
+// console.log(2+5)
+// console.log(2-5)
+// console.log(2*5)
+// console.log(6/2)
+// console.log(5%2)
+// //Reminder
+// console.log(5**3)
+
+// Assignment Operator
+
+
+
+
+// Number
+
+// let a =10;
+// let b = 235.53489;
+// let c = b.toFixed(2)
+// console.log(typeof c)
+
+// console.log(b.toString())
+
+// let c =  new Number(20)
+// let d = 20;
+// console.log(typeof b)
+
+
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+// console.log(Math.ceil(6.4));
+// console.log(Math.floor(6.4))
+// console.log(Math.log(10,30))
+// console.log(Math.max(10,2,4,12,14,7))
+// console.log(Math.random())
+// [0,1]: it will generate value between 0 and 1, where 0 is included but 1 is not included
+
+
+
+// satebaaji ek game 
+
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+// console.log(Math.floor(Math.random()*6)+1)
+// console.log(Math.floor(Math.random()*11)+15)
+//     let max =25;
+//     let min =15
+
+// console.log(Math.floor(Math.random()* (max-min +1)+min))
+
+
+
+console.log(Math.floor(Math.random()* (9999-1000 +1)+1000))
+
+
+
