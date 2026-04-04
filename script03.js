63ef4f9690c7ceedcdd1b686ea9af48e058e78f6
Prem <premkumar33652@gmail.com>
2026-04-04 23:41:49 +0530

    string date

---
+++ script03.js
@@ -0,0 +1,53 @@
+// String 
+
+// const str1 = "mohan ";
+// const str2 =  "--ohit";
+// const day = 18;
+// const str3 = `summer is comming  ${day}`
+
+
+
+const str = `Hello coder Army`
+
+// console.log(str.length);
+// console.log(str.toUpperCase())
+// console.log(str.replace("ode",'iam'))
+
+
+
+// const user = "   Rohit negi   "
+// console.log(user.trim())
+
+
+// const names  = "Rohit, Mohit, suraj, Rohan, Anjali"
+
+// console.log(names.split(" "))
+
+
+// const now = new Date();
+
+// console.log(now.toString())
+// console.log(now.toISOString())
+// console.log(now.toLocaleString())
+
+
+
+// console.log(now.getDate())
+// console.log(now.getDay())
+// console.log(now.getFullYear())
+// console.log(now.getMonth())
+// console.log(now.getHours( ))
+// console.log(now.getSeconds())
+
+
+
+// const now = new Date(2026,4,4,11,18,122);
+// console.log(now.toString())
+
+
+const now  = Date.now()
+//const dates = new Date(1775325266397);
+const dates = new Date(now);
+console.log(dates.toString())
+
+console.log(dates)
