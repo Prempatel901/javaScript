544c3dd40c1fa7561a22b92a0503a26d81696573
Prem <premkumar33652@gmail.com>
2026-05-09 23:38:42 +0530

    forEach

---
+++ script07.js
@@ -0,0 +1,106 @@
+//let arr = [1, 2, 3, 4, 5];
+
+
+//arr.forEach((num)=> console.log(num))
+
+// arr.forEach((num)=> console.log(num*num))
+
+
+// let names = ["rahul", "aman", "sita"];
+
+// names.forEach((n)=>console.log(n.toUpperCase()))
+
+
+// let nums = [10, 20, 30];
+// let sum = 0;
+// cvalue = nums.forEach((num)=>{
+//         sum+=num;
+        
+// }
+// )
+// console.log(sum)
+
+
+// let nums = [1, 2, 3, 4, 5];
+
+// nums.forEach((num)=>{
+//     if(num%2==0)
+//         console.log(num)
+// })
+
+
+// let prices = [100, 200, 300];
+// let dic
+// let price = 0
+// prices.forEach((num)=>{
+//     dis = num*10/100
+//     price = num-dis
+//     console.log(price)
+// })
+
+
+
+// let users = [
+//   { name: "rahul", age: 20 },
+//   { name: "aman", age: 17 },
+//   { name: "sita", age: 22 }
+// ];
+
+
+// users.forEach((user)=>{
+//     if(user.age > 18)
+//         console.log(user)
+// })
+
+
+// let nums = [1, 2, 3];
+
+// let newarr = []
+
+// nums.forEach((num)=>{
+//     newarr.push(num*2)
+// })
+
+// console.log(newarr)
+
+
+// let str = "hello";
+
+// let arr = str.split('')
+
+// arr.forEach((ch)=>{
+// console.log(ch)
+    
+// })
+
+
+// let nums = [5, 10, 15, 20];
+// let count = 0;
+// nums.forEach((n)=>{
+//     if(n>10)
+//         count++
+// })
+// console.log(count)
+
+
+// let words = ["apple", "banana", "mango"];
+
+// let len =[]
+// words.forEach((L)=>{
+//         len.push(L.length)
+// })
+// console.log(len)
+
+
+// let nums = [1, 2, 3, 4, 5];
+
+// nums.forEach((n,index,arr)=>{
+//     console.log(arr[arr.length - 1 -index])
+// })
+
+
+// let nums = [1, 2, 3, 4, 5];
+
+// nums.forEach((num, index, arr) => {
+//     console.log(num, index, arr);
+// });
